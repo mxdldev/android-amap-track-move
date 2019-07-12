3544612603e9044e681e0d793a4f6c4824665e28
geduo83 <geduo_83@163.com>
2019-07-12 18:42:37 +0800

    3

--- app/src/main/java/com/mxdl/movecar/MainActivity.java
+++ app/src/main/java/com/mxdl/movecar/MainActivity.java
@@ -56,6 +56,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     int b = 0;
     int c = 0;
     int d = 0;
+    int e = 0;
 
     public interface EventType {
         int MapMove = 0;// 0:地图实时跟随；1：小车停止运动；2：小车运动;3：地图加载成功;4:熄火后地图全览
